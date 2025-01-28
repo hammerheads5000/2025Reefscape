aed45e7263232a6b8a4ab9ffeedf9d6bd6de5274
LordOfFrogs <ndtech260@gmail.com>
2025-01-27 22:13:36 -0500

    Created ElevatorConstants, commands in ElevatorSubsystem

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,9 +4,48 @@
 
 package frc.robot;
 
+import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.InchesPerSecond;
+import static edu.wpi.first.units.Units.Seconds;
+import static edu.wpi.first.units.Units.Volts;
+
+import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.units.measure.LinearAcceleration;
+import edu.wpi.first.units.measure.LinearVelocity;
+import edu.wpi.first.units.measure.Voltage;
+
 public class Constants {
     public class ElevatorConstants {
-        
+        public static final Voltage MAX_OUTPUT = Volts.of(12);
+        public static final Voltage MIN_OUTPUT = Volts.of(-12);
+
+        // Motors
+        public static final int MOTOR_1_ID = 0;
+        public static final int MOTOR_2_ID = 0;
+        public static final boolean MOTOR_OPPOSE_DIRECTION = false; // whether motor 2 rotates the same direction as motor 1
+
+        // PID
+        public static final double kP = 1.0; // volts per inch error
+        public static final double kI = 0; // volts per inch-seconds error
+        public static final double kD = 0; // volts per inch per second error
+        public static final Distance TOLERANCE = Inches.of(1);
+
+        // Feedforward
+        public static final double kS = 0; // volts to overcome static friction
+        public static final double kG = 0; // volts to overcome gravity
+        public static final double kV = 0; // feedforward volts per inch per second
+        public static final double kA = 0; // feedforward volts per inch per second^2
+
+        // Trapezoid profile constraints
+        public static final LinearVelocity MAX_VELOCITY = InchesPerSecond.of(72);
+        public static final LinearAcceleration MAX_ACCELERATION = InchesPerSecond.of(72).div(Seconds.of(1));
+
+        // Setpoints
+        public static final Distance L1_HEIGHT = Inches.of(0);
+        public static final Distance L2_HEIGHT = Inches.of(0);
+        public static final Distance L3_HEIGHT = Inches.of(0);
+        public static final Distance L4_HEIGHT = Inches.of(0);
+        public static final Distance INTAKE_HEIGHT = Inches.of(0);
     }
 
 }
--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -4,10 +4,15 @@
 
 package frc.robot.subsystems;
 
+import static edu.wpi.first.units.Units.Inches;
 import static edu.wpi.first.units.Units.Meters;
+import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.MetersPerSecondPerSecond;
+import static frc.robot.Constants.ElevatorConstants.*;
 
 import com.ctre.phoenix6.controls.ControlRequest;
 import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.controls.NeutralOut;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 
@@ -16,8 +21,12 @@ import edu.wpi.first.math.controller.ElevatorFeedforward;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
 
 @Logged
 public class ElevatorSubsystem extends SubsystemBase {
@@ -26,31 +35,38 @@ public class ElevatorSubsystem extends SubsystemBase {
 
   TalonFX motor1;
   TalonFX motor2;
-  
+
   VoltageOut motorControl;
   Follower followerControl;
 
   /** Creates a new ElevatorSubsystem. */
   public ElevatorSubsystem() {
-    controller = new ProfiledPIDController(0, 0, 0, new TrapezoidProfile.Constraints(0, 0));
-    elevatorFeedforward = new ElevatorFeedforward(0, 0, 0);
-    
-    controller.setTolerance(0);
+    controller = new ProfiledPIDController(kP, kI, kD,
+        new TrapezoidProfile.Constraints(MAX_VELOCITY.in(MetersPerSecond),
+            MAX_ACCELERATION.in(MetersPerSecondPerSecond)));
 
-    followerControl.MasterID = motor1.getDeviceID();
+    elevatorFeedforward = new ElevatorFeedforward(kS, kG, kV, kA);
+
+    controller.setTolerance(TOLERANCE.in(Inches));
+
+    followerControl = new Follower(motor1.getDeviceID(), MOTOR_OPPOSE_DIRECTION);
     motor2.setControl(followerControl);
   }
 
   public Distance getHeight() {
     return Meters.of(0);
   }
-  
+
   public void setHeight(Distance height) {
     controller.setGoal(height.in(Meters));
   }
 
+  public void stop() {
+    motor1.setControl(new NeutralOut());
+  }
+
   private void controlUpdate() {
-    double output = controller.calculate(getHeight().in(Meters));
+    double output = controller.calculate(getHeight().in(Inches));
     output += elevatorFeedforward.calculate(controller.getSetpoint().velocity);
     motor1.setControl(motorControl.withOutput(output));
   }
@@ -59,4 +75,36 @@ public class ElevatorSubsystem extends SubsystemBase {
   public void periodic() {
     controlUpdate();
   }
+
+  public Command stopCommand() {
+    return this.runOnce(this::stop);
+  }
+
+  public Command goToHeightCommand(boolean instant, Distance height) {
+    if (instant) {
+      return this.runOnce(() -> setHeight(height));
+    }
+    return this.run(() -> setHeight(height)).until(() -> controller.atGoal());
+  }
+
+  public Command goToL1Command(boolean instant) {
+    return goToHeightCommand(instant, L1_HEIGHT);
+  }
+
+  public Command goToL2Command(boolean instant) {
+    return goToHeightCommand(instant, L2_HEIGHT);
+  }
+
+  public Command goToL3Command(boolean instant) {
+    return goToHeightCommand(instant, L3_HEIGHT);
+  }
+
+  public Command goToL4Command(boolean instant) {
+    return goToHeightCommand(instant, L4_HEIGHT);
+  }
+
+  public Command goToIntakePosCommand(boolean instant) {
+    return goToHeightCommand(instant, INTAKE_HEIGHT);
+  }
+
 }
