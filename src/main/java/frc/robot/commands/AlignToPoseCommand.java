e0664bf8f15c7cca77e0c032b1bbb639f5bf8c0e
LordOfFrogs <ndtech260@gmail.com>
2025-01-29 22:21:03 -0500

    Added PID to AlignToPoseCommand

--- src/main/java/frc/robot/commands/AlignToPoseCommand.java
+++ src/main/java/frc/robot/commands/AlignToPoseCommand.java
@@ -4,18 +4,36 @@
 
 package frc.robot.commands;
 
+import static edu.wpi.first.units.Units.DegreesPerSecond;
+import static edu.wpi.first.units.Units.MetersPerSecond;
 import static frc.robot.Constants.SwerveConstants.*;
 
+import com.pathplanner.lib.config.PIDConstants;
+
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.LinearVelocity;
 import edu.wpi.first.wpilibj2.command.Command;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class AlignToPoseCommand extends Command {
     Pose2d targetPose;
+    PIDController pidControllerX;
+    PIDController pidControllerY;
+    PIDController pidControllerAngle;
 
     /** Creates a new AlignToPoseCommand. */
-    public AlignToPoseCommand(Pose2d targetPose) {
+    public AlignToPoseCommand(Pose2d targetPose, PIDConstants pidConstantsX, PIDConstants pidConstantsY, PIDConstants pidConstantsAngle) {
         this.targetPose = targetPose;
+
+        pidControllerX = new PIDController(pidConstantsX.kP, pidConstantsX.kI, pidConstantsX.kD);
+        pidControllerY = new PIDController(pidConstantsY.kP, pidConstantsY.kI, pidConstantsY.kD);
+        pidControllerAngle = new PIDController(pidConstantsAngle.kP, pidConstantsAngle.kI, pidConstantsAngle.kD);
+
+        pidControllerX.setSetpoint(0);
+        pidControllerY.setSetpoint(0);
+        pidControllerAngle.setSetpoint(0);
     }
 
     // Called when the command is initially scheduled.
@@ -26,6 +44,12 @@ public class AlignToPoseCommand extends Command {
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
+        Pose2d relativePose = getPose().relativeTo(targetPose);
+        LinearVelocity xVel = MetersPerSecond.of(pidControllerX.calculate(relativePose.getTranslation().getX()));
+        LinearVelocity yVel = MetersPerSecond.of(pidControllerY.calculate(relativePose.getTranslation().getY()));
+        AngularVelocity angleVel = DegreesPerSecond.of(pidControllerAngle.calculate(relativePose.getRotation().getDegrees()));
+
+        // TODO: Swerve relative drive to xVel, yVel, angleVel
     }
 
     // Called once the command ends or is interrupted.
