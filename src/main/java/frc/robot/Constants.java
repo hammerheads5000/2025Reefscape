439e1c3184b4f96b5cd787183f64e6febaa910c9
LordOfFrogs <ndtech260@gmail.com>
2025-02-23 03:27:38 -0500

    Added new path generation for reef alignment

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -294,6 +294,7 @@ public class Constants {
     
         public static final Distance DISTANCE_TO_REEF = Inches.of(29 / 2).plus(BUMPER_THICKNESS);
         public static final Distance APPROACH_DISTANCE = Inches.of(30); // *extra* distance to reef when approaching
+        public static final Distance TRAVERSE_DISTANCE = Inches.of(60); // *extra* distance to reef when moving around to other side
 
         public static final Map<Character, Pair<Integer, Integer>> LETTER_TO_SIDE_AND_RELATIVE = Map.ofEntries(
                 Map.entry(Character.valueOf('A'), new Pair<Integer, Integer>(0, 1)),
